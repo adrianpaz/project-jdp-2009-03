40310dd97233590e231705c92abde1687da2a6cd
Kamil Zalewski <kamilzalewski89@gmail.com>
2020-09-21 14:23:38 +0200

    task-JDP200903-4

---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -0,0 +1,23 @@
+package com.kodilla.ecommercee.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDto {
+
+    private Long id;
+    private List<ProductDto> orderedProducts;
+
+    public OrderDto(Long id, List<ProductDto> orderedProducts) {
+        this.id = id;
+        this.orderedProducts = new ArrayList<>(orderedProducts);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<ProductDto> getOrderedProducts() {
+        return orderedProducts;
+    }
+}
