95a79e682b3c459a6c945d040d10831155d8edae
Patrycja05 <patrycja.calka@wp.pl>
2020-09-21 17:50:41 +0200

    Cart Controller initial implementation - poprawki

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,6 +1,7 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.CartDto;
+import com.kodilla.ecommercee.domain.OrderDto;
 import com.kodilla.ecommercee.domain.ProductDto;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,16 +14,16 @@ import java.util.List;
 public class CartController {
 
     @RequestMapping(method = RequestMethod.POST, value = "createCart")
-    public void createCart(@RequestBody CartDto cartDto){
+    public void createCart(){
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
-    public List<ProductDto> getProducts(@RequestParam List<ProductDto> productDtoList){
-        return productDtoList;
+    @RequestMapping(method = RequestMethod.GET, value = "getCart")
+    public List<ProductDto> getCart(@RequestParam Long cartId){
+        return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "addProduct")
-    public void addProductToCart(@RequestBody ProductDto productDto){
+    @RequestMapping(method = RequestMethod.PUT, value = "addProductToCart")
+    public void addProductToCart(@RequestParam Long productId){
         List<ProductDto> cart = new ArrayList<>();
         cart.add(new ProductDto(1L, "Neckles", "silver", new BigDecimal(100), "3"));
         cart.add(new ProductDto(2L, "T-shirt", "cotton", new BigDecimal(50), "1"));
@@ -31,10 +32,10 @@ public class CartController {
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
     public void deleteProduct(@RequestParam Long productId){
-        
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "createOrder")
-    public void createOrder(@RequestBody CartDto cartDto){
+    public OrderDto createOrder(@RequestBody CartDto cartDto){
+        return new OrderDto();
     }
 }
--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -1,49 +1,18 @@
 package com.kodilla.ecommercee.domain;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CartDto {
     private Long cartId;
-    private String name;
-    private String description;
-    private BigDecimal price;
-    private String groupId;
-    private Long orderId;
     private List<ProductDto> productDtoList = new ArrayList<>();
 
-    public CartDto(Long cartId, String name, String description, BigDecimal price, String groupId, Long orderId) {
+    public CartDto(Long cartId) {
         this.cartId = cartId;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.groupId = groupId;
-        this.orderId = orderId;
     }
 
     public Long getCartId() {
         return cartId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public Long getOrderId() {
-        return orderId;
     }
 
     public List<ProductDto> getProductDtoList() {
