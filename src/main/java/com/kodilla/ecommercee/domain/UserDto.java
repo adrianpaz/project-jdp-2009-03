915b8d32b842e78083e3aedd0147c17453111c98
ewelinalimanska <limanskaewelina@gmail.com>
2020-10-07 16:38:18 +0200

    User - usunięcie Lomboka

--- src/main/java/com/kodilla/ecommercee/domain/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/UserDto.java
@@ -1,10 +1,5 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class UserDto {
 
     private Long id;
@@ -12,4 +7,26 @@ public class UserDto {
     private String status;
     private Long userKey;
 
+    public UserDto(Long id, String userName, String status, Long userKey) {
+        this.id = id;
+        this.userName = userName;
+        this.status = status;
+        this.userKey = userKey;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Long getUserKey() {
+        return userKey;
+    }
 }
