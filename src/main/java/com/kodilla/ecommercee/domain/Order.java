79f240bf59451986cdfd71a2078d3edde0b02af6
ewelinalimanska <limanskaewelina@gmail.com>
2020-10-11 22:54:37 +0200

    Dodanie settera

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -22,7 +22,6 @@ public class Order {
     public Order() {
     }
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @NotNull
@@ -46,6 +45,10 @@ public class Order {
     @JoinColumn(name = "userId")
     public Long getUserId() {
         return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     @ManyToOne
